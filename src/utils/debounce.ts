b06f0cb25d9061b5073c628f35766eed61e11f35
Zavins <zywang1@uci.edu>
2023-12-28 14:23:55 -0800

    Add helper function: debounce

---
+++ src/utils/debounce.ts
@@ -0,0 +1,13 @@
+const debounce = (func: (...args: any) => void, delay: number) => {
+    let timer: ReturnType<typeof setTimeout>
+    return function (this: any, ...args: any) {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            func.apply(this, args)
+        }, delay)
+    }
+}
+
+export default debounce
