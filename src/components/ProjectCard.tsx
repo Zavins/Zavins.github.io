a8135092624b4c74c865e9e47bae41e14efc6d8d
Zavins <zywang1@uci.edu>
2024-01-03 00:21:11 -0800

    Fix project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,43 +8,48 @@ import {
     CardOverflow,
     Typography,
 } from "@mui/joy"
-import { useState } from "react"
-import ProjectDialog from "./ProjectDialog"
+import intl from "react-intl-universal"
 
-const ProjectCard = () => {
-    const [projectDialogOpen, setProjectDialogOpen] = useState(false)
+interface ProjectCardProps {
+    title: string
+    description: string
+    img: {
+        src: string
+        alt?: string
+    }
+    github: string
+    onClickViewDetail: () => void
+}
 
+const ProjectCard = (props: ProjectCardProps) => {
+    const { title, description, img, github, onClickViewDetail } = props
     return (
         <Card variant="soft" sx={{ width: 320 }}>
             <CardOverflow>
                 <AspectRatio ratio="2">
-                    <img
-                        src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
-                        loading="lazy"
-                        alt=""
-                    />
+                    <img src={img.src} loading="lazy" alt={img.alt} />
                 </AspectRatio>
             </CardOverflow>
             <CardContent sx={{ padding: 0 }}>
-                <Typography level="title-lg" textAlign="center">
-                    Fablix Movie
+                <Typography level="title-lg" textAlign="center" fontWeight="800">
+                    {title}
                 </Typography>
-                <Typography level="body-sm">
-                    A movie store website similar to Netflix. It contains user login, movie search,
-                    and checkout page.{" "}
-                </Typography>
+                <Typography level="body-sm">{description}</Typography>
             </CardContent>
             <CardActions sx={{ justifyContent: "space-between" }}>
-                <Button variant="solid" color="neutral" endDecorator={<GitHub />}>
-                    GitHub
+                <Button
+                    variant="solid"
+                    component="a"
+                    href={github}
+                    target="_blank"
+                    color="neutral"
+                    endDecorator={<GitHub />}
+                >
+                    {intl.get("GITHUB")}
                 </Button>
-                <Button variant="solid" size="md" onClick={() => setProjectDialogOpen(true)}>
-                    View Detail
+                <Button variant="solid" size="md" onClick={onClickViewDetail}>
+                    {intl.get("VIEW_DETAIL")}
                 </Button>
-                <ProjectDialog
-                    open={projectDialogOpen}
-                    onClose={() => setProjectDialogOpen(false)}
-                />
             </CardActions>
         </Card>
     )
